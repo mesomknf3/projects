1a150fc69222ac0b087702c0a994fcec2255daad
mesomknf3 <72368636+mesomknf3@users.noreply.github.com>
2020-10-05 21:41:13 +0530

    Add files via upload

---
+++ dollar best.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	int q,di,n,p;
+	
+	cout<<"Enter amount: ";
+	cin>>nu
+
+	d=num/100;
+	num=num;
+	n=num/5;
+	num=num-n*5;
+	p=num;
+	
+	cout<<"Dollar: "<<d<<end
+	cout<<"Nickle: "<<n<<endl;
+	cout<<"Penny: "<<p<<endl;
+	return 0;
+}
